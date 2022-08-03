420f2245d3ee44a9a38d926833ab46541493af65
dilaouid <dilaouid@student.42.fr>
2022-08-04 01:36:35 +0200

    (+) catch API call

--- src/ApiMovie.js
+++ src/ApiMovie.js
@@ -2,11 +2,21 @@
 const API_KEY = process.env.REACT_APP_API_KEY
 const API_URL = "https://api.themoviedb.org/3/"
 
+const emptyObject = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+};
+
 const fetchMovies = async (endpoint) => {
   return await fetch(
     `${API_URL}${endpoint}?language=fr-FR&api_key=${API_KEY}`
-  ).then((response) => response.json())
-}
+  ).then((response) => response.json()).catch((e) => {
+    console.error(e);
+    return (emptyObject);
+  });
+};
 
 export default {
   getHomeMovies: async () => {
